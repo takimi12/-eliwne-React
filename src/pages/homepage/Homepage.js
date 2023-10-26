4639e031cc2ea20240ddf80e8c4146e5bcac7611
takimi12 <tomek12olech@gmail.com>
2023-10-27 00:29:40 +0200

    Future Section

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -3,6 +3,7 @@ import Renovation from "./renovation/Renovation";
 import Opinion from "./Opinion/Opinion";
 import Inspirattion from "./inspiration/Inspiration";
 import Featured from "./Featured/Featured";
+import FutureSection from "./FutureSection/FutureSection";
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
        {/* <Hero />  
             <MostPopular /> */}
             <Inspirattion />
-            {/* <FutureSection /> */}
+            <FutureSection />
             <Renovation />
             <Opinion />
             <Featured />
