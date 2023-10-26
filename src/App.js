59b76272b5a25fcd05805fa290570a00e10176ed
takimi12 <tomek12olech@gmail.com>
2023-10-27 01:13:47 +0200

    Contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route,  Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Homepage from "./pages/homepage/Homepage";
+import Contact from "./pages/contacrt/Contact";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
 
         <Route path='/' element={<Homepage />} />
+        <Route path='/Contact' element={<Contact />} />
         </Routes>
       </Router>
       <Footer />
